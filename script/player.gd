4aa07052a9a7f3b31f6997870450409c072a7ea8
Tacioli21 <enzo.tazioli@uscsonline.com.br>
2025-08-09 11:24:45 -0300

    0.1.1
    Enemy

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 const SPEED: float = 150.0
-
+var buly = Input.get_axis("shoot up", "shoot down");
+var bulx = Input.get_axis("shoot left", "shoot right");
+var desiredBullet = Vector2 (bulx, buly)
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
@@ -15,6 +17,8 @@ func _physics_process(_delta: float) -> void:
 	var dirX = Input.get_axis("move left","move right");
 	var desiredDirection = Vector2( dirX, dirY );
 	
+	
+	
 	if desiredDirection:
 		velocity = desiredDirection.normalized() * SPEED;
 	else:
@@ -23,8 +27,12 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide();
 
 func _set_player_animation(desiredDirection: Vector2) -> void:
-	if desiredDirection.y == 1:
+	if desiredDirection.y == 1 and desiredBullet.y == 1:
+		animated_sprite_2d.play("shoot_left")
+	elif desiredDirection.y == 1:
 		animated_sprite_2d.play("walk_front")
+
+
 	elif desiredDirection.y == -1:
 		animated_sprite_2d.play("walk_back")
 	elif desiredDirection.x == 1:
