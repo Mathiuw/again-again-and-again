3407719363af38dd40285bdd496ea37ff1f409b6
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-12-08 21:56:56 -0300

    level layout placeholder, chest WIP, boss_lynx WIP

--- script/door.gd
+++ script/door.gd
@@ -2,9 +2,14 @@ class_name Door
 extends Area2D
 
 @export var desired_room: Room
+@export var always_open: bool = false
 @onready var desired_position: Marker2D = $DesiredPosition
 @onready var door_static_body: StaticBody2D = $DoorStaticBody
 @onready var open_particle: GPUParticles2D = $OpenParticle
+
+func  _ready() -> void:
+	if  always_open:
+		set_door_open_state(true)
 
 func _on_body_entered(body: Node2D) -> void:
 	if desired_room && body is Player:
@@ -17,6 +22,8 @@ func set_door_open_state(state: bool) -> void:
 		door_static_body.process_mode = Node.PROCESS_MODE_DISABLED
 		open_particle.emitting = true
 	else:
+		if always_open:
+			return
 		door_static_body.show()
 		door_static_body.process_mode = Node.PROCESS_MODE_INHERIT
 		open_particle.emitting = false
