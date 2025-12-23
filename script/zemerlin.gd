95979b4e47086470d635216c1bb2af8f1c1ed25e
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-12-23 12:36:55 -0300

    zemerlin animations

--- script/zemerlin.gd
+++ script/zemerlin.gd
@@ -10,6 +10,7 @@ var target: Node2D = null
 
 @onready var _health: Health = $HealthComponent
 @onready var _navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var _animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	# Die function connect
@@ -18,6 +19,11 @@ func _ready() -> void:
 	target = get_tree().get_first_node_in_group("player")
 	if  !target:
 		push_error("Couldnt find player")
+
+
+func _process(_delta: float) -> void:
+	print(velocity.normalized())
+	set_animations()
 
 
 func _physics_process(_delta: float) -> void:
@@ -39,10 +45,42 @@ func _physics_process(_delta: float) -> void:
 	else:
 		_on_navigation_agent_2d_velocity_computed(new_velocity)
 
+
+func _set_zemerlin_animation(desiredDirection: Vector2) -> void:
+	var margin: float = 0.4
+	
+	if desiredDirection.y <= margin && desiredDirection.y >= -margin && desiredDirection.x > 0:
+		_animated_sprite_2d.play("walk_right")
+	elif desiredDirection.y <= margin && desiredDirection.y >= -margin && desiredDirection.x < 0:
+		_animated_sprite_2d.play("walk_left")
+	elif desiredDirection.y > margin:
+		_animated_sprite_2d.play("walk_front")
+	elif desiredDirection.y < margin:
+		_animated_sprite_2d.play("walk_back")
+
+
+func _set_zemerlin_idle() -> void:
+	match _animated_sprite_2d.animation:
+		"walk_back":
+			_animated_sprite_2d.play("idle_back")
+		"walk_front":
+			_animated_sprite_2d.play("idle_front")
+		"walk_left":
+			_animated_sprite_2d.play("idle_left")
+		"walk_right":
+			_animated_sprite_2d.play("idle_right")
+
 # Timer calculate cooldown
 func _on_draw_path_timer_timeout() -> void:
 	if target:
 		_navigation_agent_2d.target_position = target.global_position
+
+
+func set_animations() -> void:
+	if velocity.length() > 0:
+		_set_zemerlin_animation(velocity.normalized())
+	else:
+		_set_zemerlin_idle()
 
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
